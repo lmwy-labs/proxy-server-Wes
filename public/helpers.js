5abac2a808f33ad0c5757aa3e99ea7087b202ddc
Michael Kim <miketkim@umich.edu>
2019-09-25 13:03:20 -0700

    3 bundles on proxy

--- public/helpers.js
+++ public/helpers.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 let getReviewsbyID = (id, callback) => {
   let options = {
-    url: `http://localhost:3001/restaurants/${id}`
+    url: `http://52.8.6.53:3001/restaurants/${id}`
   };
 
   request.get(options, function(err, reviewsObj) {
@@ -16,4 +16,34 @@ let getReviewsbyID = (id, callback) => {
 
 }
 
-module.exports.getReviewsbyID = getReviewsbyID;
+let getReservationsbyID = (id, callback) => {
+    const r_id = id.substring(1);
+    let options = {
+      url: `http://54.215.234.24:3003/restaurants/${r_id}`
+    };
+  
+    request.get(options, function(err, reservationsObj) {
+      if (err) {
+        console.error(err, null);
+      } else {
+        console.log(null, reservationsObj.body);
+        callback(reservationsObj);
+      }
+    });
+  }
+
+  let getPhotosbyID = (id, callback) => {
+    let options = {
+      url: `http://54.193.42.82:3002/restaurants/${id}`
+    };
+    request.get(options, function(err, photosObj) {
+      if (err) {
+        console.error(err, null);
+      } else {
+        console.log(null, photosObj.body);
+        callback(photosObj);
+      }
+    });
+  }
+
+module.exports = { getReviewsbyID, getReservationsbyID, getPhotosbyID };
